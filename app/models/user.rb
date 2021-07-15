d47252d6673680fa579e24aedc215c693b944904
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-16 08:46:29 +0900

    ルボコップ

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-         
+
   validates :nickname,   presence: true
   validates :first_name, presence: true
   validates :first_read, presence: true
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {'山田'}
-    first_name            {'太郎'}
-    last_read             {'ヤマダ'}
-    first_read            {'タロウ'}
-    birthday              {'19300101'}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太郎' }
+    last_read             { 'ヤマダ' }
+    first_read            { 'タロウ' }
+    birthday              { '19300101' }
   end
 end
