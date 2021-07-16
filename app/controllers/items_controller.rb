fa615e306ed289774bd5435cc75bd61cffe33e05
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-16 17:12:35 +0900

    カラム作成・アクティブストレージ導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    # @items = Item.all
+     @items = Item.all
+     @item = Itme.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
+
 end
