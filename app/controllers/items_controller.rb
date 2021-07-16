b15316cac4dd5918e5e9966fd103c070d0dac8b7
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-16 20:19:59 +0900

    商品作成プルダウン

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
@@ -15,7 +19,7 @@ class ItemsController < ApplicationController
 
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name, :category_id, :price, :user, :status_id, :delivery_id, :description, :area_id, :ship_id).merge(user_id: current_user.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
   belongs_to :user
+  belongs_to :aera
+  belongs_to :delivery
+  belongs_to :ship
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :price
     validates :user
-    validates :status_id
-    validates :delivery_id
-    validates :area_id
-    validates :ship_id
+    validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+    
   end
 end
