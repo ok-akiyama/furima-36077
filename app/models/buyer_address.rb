073738d5af8fb009a184ab0cadf7cca19e7be689
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-19 23:03:40 +0900

    クレジット決済機能

---
+++ app/models/buyer_address.rb
@@ -0,0 +1,4 @@
+class BuyerAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :price, :user_id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+  resources :orders, only:[:create]
   resources :items
   #resources :users, only: [:index, :create, ]
 end
