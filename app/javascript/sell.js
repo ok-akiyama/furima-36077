6d7929e7844ffee55a74ac0c8b91dd62bea61dd5
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-27 17:22:08 +0900

    card.jsとsell.jsの該当ページ以外のエラー解消

--- app/javascript/sell.js
+++ app/javascript/sell.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+  if ( document.getElementById('item-price')){
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
      const addTaxDom = document.getElementById("add-tax-price");
@@ -6,4 +7,5 @@ window.addEventListener('load', () => {
      const addPofitDom = document.getElementById("profit");
      addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
  })
+}
 });
